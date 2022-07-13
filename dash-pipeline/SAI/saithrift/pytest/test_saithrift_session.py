d89e1d426d59b8296972e56362f30b4cf22da07d
Chris Sommers <chrispsommers@gmail.com>
2022-07-13 16:30:16 -0700

    Refactor pytest to use common theift Client class; run sai client tests in CI. NOte - need sai_api_query impl to avoid sai thrift server crashes.

--- dash-pipeline/SAI/saithrift/pytest/test_saithrift_session.py
+++ dash-pipeline/SAI/saithrift/pytest/test_saithrift_session.py
@@ -1,7 +1,6 @@
 import pytest
 import snappi
 import thrift
-import sai_thrift
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
@@ -11,12 +10,10 @@ import sai_thrift.sai_adapter as adapter
 
 THRIFT_PORT = 9092
 
-class TestSaiThrift:
-
-    def test_saithrift_session(self):
+class SaithriftClient:
+    def __init__(self):
         self.transport = None
         self.createRpcClient()
-        print ("test_saithrift_session")
 
     def createRpcClient(self):
         """
@@ -28,9 +25,22 @@ class TestSaiThrift:
         # else:
         server = 'localhost'
 
+        print ("making thrift connection to %s:%d" % (server, THRIFT_PORT))
         self.transport = TSocket.TSocket(server, THRIFT_PORT)
         self.transport = TTransport.TBufferedTransport(self.transport)
         self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
 
         self.client = sai_rpc.Client(self.protocol)
         self.transport.open()    
+        print ("sai-thrift connection established")
+       
+class TestSaiThrift():
+    def test_saithrift_session(self):
+        client = SaithriftClient().client
+        print ("test_saithrift_session OK")
+
+    # def test_sai_thrift_get_switch_attribute(self):
+    #     client = SaithriftClient().client
+    #     attr = adapter.sai_thrift_get_switch_attribute(
+    #         client, number_of_active_ports=True)
+
