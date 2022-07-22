1a611e24ae8a9a16788ae1f6c3996f4127e8f604
Chris Sommers <chrispsommers@gmail.com>
2022-07-21 18:02:57 -0700

    Modify vnet pytest case to create/delete. README. Makefile polishing.

--- dash-pipeline/SAI/saithrift/pytest/vnet/test_saithrift_vnet.py
+++ dash-pipeline/SAI/saithrift/pytest/vnet/test_saithrift_vnet.py
@@ -10,8 +10,11 @@ from sai_thrift.ttypes  import *
 @pytest.mark.vnet
 def test_sai_thrift_create_outbound_eni_to_vni_entry(saithrift_client):
     dle = sai_thrift_direction_lookup_entry_t(switch_id=0, vni=60)
-    sai_thrift_create_direction_lookup_entry(saithrift_client, dle,
+    status = sai_thrift_create_direction_lookup_entry(saithrift_client, dle,
                         action=SAI_DIRECTION_LOOKUP_ENTRY_ACTION_SET_OUTBOUND_DIRECTION)
+    assert(status == SAI_STATUS_SUCCESS)
+    status = sai_thrift_remove_direction_lookup_entry(saithrift_client, dle)
+    assert(status == SAI_STATUS_SUCCESS)                        
+    
     print ("test_sai_thrift_create_outbound_eni_to_vni_entry OK")
-        
-
+    
