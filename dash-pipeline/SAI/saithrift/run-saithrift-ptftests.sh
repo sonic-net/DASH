b3a2debcaca80532c0ad5f9f385defb98fc6b0da
Chris Sommers <chrispsommers@gmail.com>
2022-07-24 11:32:51 -0700

    Moved saithrift tests under tests/ for consistency.

--- dash-pipeline/SAI/saithrift/run-saithrift-ptftests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo ptf --test-dir ptf/ --pypath ${PWD}/../../SAI/ptf \
-	 --interface 0@veth1 --interface 1@veth3
---
+++ dash-pipeline/tests/saithrift/run-saithrift-ptftests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# To be run inside saithrift-client container, assumes SAI repo portions exist under /SAI directory
+sudo ptf --test-dir ptf/ --pypath /SAI/ptf \
+	 --interface 0@veth1 --interface 1@veth3
