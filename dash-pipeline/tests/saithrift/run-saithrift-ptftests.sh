2d866fc61f48aaa532ea2a69028286bb0de92146
Chris Sommers <chrispsommers@gmail.com>
2022-07-24 14:21:34 -0700

    FIx path, paste errors which run tests.

--- dash-pipeline/tests/saithrift/run-saithrift-ptftests.sh
+++ dash-pipeline/tests/saithrift/run-saithrift-ptftests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # To be run inside saithrift-client container, assumes SAI repo portions exist under /SAI directory
-sudo ptf --test-dir ptf/ --pypath /SAI/ptf \
+sudo ptf --test-dir ./saithrift/ptf/ --pypath /SAI/ptf \
 	 --interface 0@veth1 --interface 1@veth3
