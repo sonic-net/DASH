32d63e6911402cb063eb2a3f1aee9dbd116c4e25
Maksym Hedeon <106111454+maksymhedeon@users.noreply.github.com>
2022-08-31 10:15:19 +0300

    Added SAI-Challenger to CI setup.
        1. Added SAI-Challenger submodule.
        2. Added SAI-Challenger basic test.
        3. Changed Makefile to build/start/stop SAI-Challenger.
    
    Signed-off-by: Maksym Hedeon <maksym.hedeon@plvision.eu>

---
+++ test/test-cases/test_vector_example/conftest.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import sys
+import traceback
+import pytest
+from sai_environment import init_setup
+
+def pytest_addoption(parser):
+    parser.addoption("--traffic", action="store_true", default=False, help="run tests with traffic")
+    parser.addoption("--loglevel", action="store", default='NOTICE', help="syncd logging level")
+    parser.addoption("--setup", action="store", default=None, help="Setup description (Path to the json file).")
+
+
+@pytest.fixture(scope="session")
+def exec_params(request):
+    config_param = {}
+    config_param["setup"] = init_setup(request.config)
+    config_param["traffic"] = request.config.getoption("--traffic")
+    config_param["loglevel"] = request.config.getoption("--loglevel")
+    logging.getLogger().setLevel(getattr(logging, config_param["loglevel"].upper(), "INFO"))
+    return config_param
+
+@pytest.fixture(scope="session")
+def dpu(exec_params):
+    dpu = exec_params["setup"]["DPU"][0]
+    if dpu is not None:
+        dpu.reset()
+    return dpu
+
+@pytest.fixture(scope="session")
+def dataplane_session(exec_params):
+    dataplane = exec_params["setup"]["DATAPLANE"][0]
+    # Set up the dataplane
+    dataplane.init()
+    yield dataplane
+    # Shutdown the dataplane
+    dataplane.remove()
+
+
+@pytest.fixture(scope="function")
+def dataplane(dataplane_session):
+    dataplane_session.setUp()
+    yield dataplane_session
+    dataplane_session.tearDown()
---
+++ test/test-cases/test_vector_example/test_dpu_basic.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_dpu_basic(dpu, dataplane):
+    pass
+    #TODO
