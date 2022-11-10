70e4958e0932966b8d003b6e6ecd13f40a60cc0a
Chris Sommers <chrispsommers@gmail.com>
2022-11-10 13:59:12 -0800

    Fix issue 235 - added more switch and port attributes to bmv2 libsai to satisfy initialization requirements for PTF and Pytest frameworks. Modified PTF tests to use correct base class which exploits these attributes. Removed one "chmod" command in Makefile which caused repeated "make all" to fail (discovered while implementing this fix).

--- dash-pipeline/tests/saithrift/ptf/thrift/test_thrift_session.py
+++ dash-pipeline/tests/saithrift/ptf/thrift/test_thrift_session.py
@@ -1,7 +1,7 @@
 from sai_thrift.sai_headers import *
 from sai_base_test import *
 
-class TestSaiThriftSession(ThriftInterfaceDataPlane):
+class TestSaiThriftSession(SaiHelperSimplified):
     """ Test saithrift client connection only"""
     def setup(self):
         print ("setup()")
@@ -13,65 +13,7 @@ class TestSaiThriftSession(ThriftInterfaceDataPlane):
     def teardown(self):
         print ("teardown()")
 
-# TODO - need to implement some DASH switch APIs to get switch attributes etc.
-# We need this to run traditional PTF tests which depend upon device attributes etc.
-
-# class TestSaiThriftSaiHelper(SaiHelper):
-
-#  Using SaiHelper base class, we get this (because vlan default = 0)
-# root@chris-z4:/saithrift-host# ./run-saithrift-ptftests.sh 
-# /usr/local/lib/python3.8/dist-packages/ptf-0.9.1-py3.8.egg/EGG-INFO/scripts/ptf:19: DeprecationWarning: the imp module is deprecated in favour of importlib; see the module's documentation for alternative uses
-#   import imp
-# test_thrift_session.TestThriftSession ... ***** Number of available resources *****
-# SAI_SWITCH_ATTR_ECMP_MEMBERS :  0
-# ecmp_members :  0
-# SAI_SWITCH_ATTR_NUMBER_OF_ECMP_GROUPS :  0
-# number_of_ecmp_groups :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV4_ROUTE_ENTRY :  0
-# available_ipv4_route_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV6_ROUTE_ENTRY :  0
-# available_ipv6_route_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV4_NEXTHOP_ENTRY :  0
-# available_ipv4_nexthop_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV6_NEXTHOP_ENTRY :  0
-# available_ipv6_nexthop_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV4_NEIGHBOR_ENTRY :  0
-# available_ipv4_neighbor_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPV6_NEIGHBOR_ENTRY :  0
-# available_ipv6_neighbor_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_NEXT_HOP_GROUP_ENTRY :  0
-# available_next_hop_group_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_NEXT_HOP_GROUP_MEMBER_ENTRY :  0
-# available_next_hop_group_member_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_FDB_ENTRY :  0
-# available_fdb_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_IPMC_ENTRY :  0
-# available_ipmc_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_SNAT_ENTRY :  0
-# available_snat_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_DNAT_ENTRY :  0
-# available_dnat_entry :  0
-# SAI_SWITCH_ATTR_AVAILABLE_DOUBLE_NAT_ENTRY :  0
-# available_double_nat_entry :  0
-# FAIL
-
-# ======================================================================
-# FAIL: test_thrift_session.TestThriftSession
-# ----------------------------------------------------------------------
-# Traceback (most recent call last):
-#   File "/saithrift-host/../../SAI/ptf/sai_base_test.py", line 654, in setUp
-#     super(SaiHelper, self).setUp()
-#   File "/saithrift-host/../../SAI/ptf/sai_base_test.py", line 304, in setUp
-#     self.assertNotEqual(self.default_vlan_id, 0)
-# AssertionError: 0 == 0
-
-# ----------------------------------------------------------------------
-# Ran 1 test in 0.021s
-
-# FAILED (failures=1)
-
-# TODO - temporary until fix per above:
-class TestSaiThriftSaiHelper(ThriftInterfaceDataPlane):
+class TestSaiThriftSaiHelper(SaiHelperSimplified):
     """ Test saithrift client connection and basic SaiHelper intitialization"""
     def setup(self):
         print ("setup()")
