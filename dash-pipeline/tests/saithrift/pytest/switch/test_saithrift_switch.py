b6cd3825a360fc351830a612d864688a7c89de36
Chris Sommers <chrispsommers@gmail.com>
2022-11-11 09:36:24 -0800

    Changed assertions on switch attributes to check for "not None" instead of explicit values, which will allow for different results on various targets.

--- dash-pipeline/tests/saithrift/pytest/switch/test_saithrift_switch.py
+++ dash-pipeline/tests/saithrift/pytest/switch/test_saithrift_switch.py
@@ -21,30 +21,31 @@ def test_sai_thrift_get_switch_attribute(saithrift_client):
     assert(number_of_active_ports == attr['port_list'].count)
     port_list = attr['port_list'].idlist
     print ("port list = ", port_list)
+    assert(port_list  is not None)
 
     attr = sai_thrift_get_switch_attribute(
         saithrift_client, default_vlan_id=True)
     default_vlan_id = attr['default_vlan_id']
     print ("default_vlan_id = %d" % default_vlan_id)
-    assert(default_vlan_id != 0)
+    assert(default_vlan_id  is not None)
 
     attr = sai_thrift_get_switch_attribute(
         saithrift_client, default_virtual_router_id=True)
     default_virtual_router_id = attr['default_virtual_router_id']
     print ("default_virtual_router_id = %d" % default_virtual_router_id)
-    assert(default_virtual_router_id != 0)
+    assert(default_virtual_router_id  is not None)
     
     attr = sai_thrift_get_switch_attribute(
         saithrift_client, cpu_port=True)
     cpu_port = attr['cpu_port']
     print ("cpu_port = %d" % cpu_port)
-    assert(cpu_port != 0)
+    assert(cpu_port is not None)
     
     attr = sai_thrift_get_port_attribute(
         saithrift_client, cpu_port, qos_number_of_queues=True)
     qos_number_of_queues = attr['qos_number_of_queues']
     print ("qos_number_of_queues = %d" % qos_number_of_queues)
-    assert(qos_number_of_queues == 0)
+    assert(qos_number_of_queues is not None)
     
     print ("test_sai_thrift_get_switch_attribute OK")
 
