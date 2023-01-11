1b8055a1128b37969e1f7de3f681e953d9ab9b44
Anton Putria <antonx.putria@intel.com>
2023-01-11 19:09:18 +0200

    Updated run-tests script and functional/ptf tests (#314)
    - Added a new option to set exact test target name
    - Added a new option to control traffic direction in tests
    - Updated skipifs
    
    Signed-off-by: Yuriy Harhas <yuriyx.harhas@intel.com>

--- test/test-cases/functional/ptf/run-tests.sh
+++ test/test-cases/functional/ptf/run-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # To be run inside saithrift-client container, assumes SAI repo portions exist under /SAI directory
 
-SCRIPT=$0
-BMV2="True"
-TEST_DIR="functional/ptf"
+SCRIPT=$(basename $0)
+TARGET="bmv2"  # TODO: add no target by default and remove -H
+TRAFFIC="bidir"
+TEST_DIR="$(dirname $0)"
 EXTRA_PARAMETERS=""
 INTERFACES="veth1,veth3"
 TIMEOUT=1800
@@ -20,6 +21,8 @@ function show_help_and_exit()
     echo "    -U                      : specify to run PTF underlay test cases. (default: run PTF overlay test cases)"
     echo "    -I <interfaces>         : specify comma-separated DUT 2 interfaces names (default: veth1,veth3)"
     echo "    -H                      : specify if tests are run on real hardware"
+    echo "    -T <target>             : specify any specific target platform (default: bmv2)"
+    echo "    -b <traffic type>       : specify traffic type for verification: bidir|monodir|no (default: bidir)"
     echo "    -e <parameters>         : specify extra parameter(s) (default: none)"
     echo -e "    -t <timeout>            : specify timeout for test case execution\n"
 
@@ -41,7 +44,7 @@ function validate_parameters()
     fi
 }
 
-while getopts "h?UHI:s:p:c:e:t:" opt; do
+while getopts "h?UHT:I:s:p:c:b:e:t:" opt; do
     case ${opt} in
         h|\? )
             show_help_and_exit 0
@@ -49,8 +52,11 @@ while getopts "h?UHI:s:p:c:e:t:" opt; do
         U )
             TEST_DIR="/SAI/ptf"
             ;;
+        T )
+            TARGET=${OPTARG}
+            ;;
         H )
-            BMV2="False"
+            TARGET=""
             ;;
         I )
             INTERFACES=${OPTARG}
@@ -63,6 +69,9 @@ while getopts "h?UHI:s:p:c:e:t:" opt; do
             ;;
         c )
             TEST_PARAMS="${TEST_PARAMS}port_config_ini='${OPTARG}';"
+            ;;
+        b )
+            TRAFFIC=${OPTARG}
             ;;
         e )
             EXTRA_PARAMETERS="${EXTRA_PARAMETERS} ${OPTARG}"
@@ -83,5 +92,5 @@ ptf \
     --interface 0@${iface_array[0]} \
     --interface 1@${iface_array[1]} \
     --test-case-timeout=${TIMEOUT} \
-    --test-params="bmv2=${BMV2};${TEST_PARAMS}" \
+    --test-params="target='${TARGET}';traffic_check='${TRAFFIC}';${TEST_PARAMS}" \
     ${EXTRA_PARAMETERS}
