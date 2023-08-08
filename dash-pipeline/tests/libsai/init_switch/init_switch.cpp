666da4e4c7a60f4f0657366d9e9d4d6ffad739b1
Kamil Cudnik <kcudnik@gmail.com>
2023-08-08 20:39:08 +0200

    [SAI] Fix NextObjIndex method to embed objeect type into generated OID (#420)
    * [SAI] Embed object type inside object id
    
    This is required for proper working of sai_object_type_query api
    
    * Update init switch test
    
    * Use proper function name
    
    * Make proper oid

--- dash-pipeline/tests/libsai/init_switch/init_switch.cpp
+++ dash-pipeline/tests/libsai/init_switch/init_switch.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
 #include "utils.h"
 
-/*
-   Run this program to indirectly cause simple_switch_grpc to load its P4 pipeline
- */
+#include <iostream>
+
+// Run this program to indirectly cause simple_switch_grpc to load its P4 pipeline
+
 int main(int argc, char **argv)
 {
-    // Make one benign call into libsai; it will force library load and
-    // invoke static lib initializer Init() which calls SetForwardingPipelineConfigRequest()
-    GetDeviceId();
-    std::cout << "Switch is initialized." << std::endl;
+    // try initialize SAI api, this will load sai library and initialize P4 GRPC
 
-    return 0;
+    sai_status_t status = sai_api_initialize(0, nullptr);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        std::cout << "sai_api_initialize success" << std::endl;
+
+        return 0;
+    }
+
+    std::cout << "sai_api_initialize failed: " << status << std::endl;
+
+    return 1;
 }
