5648a4059e6c23666e72847f48c28c9d3c62a777
Saad Mazhar <smazhar@dreambigsemi.com>
2023-09-01 01:06:52 +0500

    Sync with main

--- dash-pipeline/python_model/dash_underlay.py
+++ dash-pipeline/python_model/dash_underlay.py
@@ -1,9 +1,22 @@
 from dash_headers import *
 from __table import *
 
+# The values in this context have been sourced from the 'saiswitch.h' file and 
+# have been manually designated to maintain alignment with enum values specified in the SAI commit <d8d40b4>.
+SAI_PACKET_ACTION_DROP = 0
+SAI_PACKET_ACTION_FORWARD = 1
+
 # Send packet on different/same port it arrived based on routing
 def set_nhop(next_hop_id : Annotated[int, 9]):
     standard_metadata.egress_spec = next_hop_id
+
+def pkt_act(packet_action: Annotated[int, 9], next_hop_id: Annotated[int, 9]):
+    if packet_action == SAI_PACKET_ACTION_DROP:
+        # Drops the packet
+        meta.dropped = True
+    elif packet_action == SAI_PACKET_ACTION_FORWARD:
+        # Forwards the packet on different/same port it arrived based on routing
+        set_nhop(next_hop_id)
 
 # Send packet on same port it arrived (echo) by default
 def def_act():
@@ -15,7 +28,7 @@ underlay_routing = Table(
         "meta.dst_ip_addr" : LPM
     },
     actions = [
-        set_nhop,
+        pkt_act,
         def_act
     ],
     default_action=def_act
