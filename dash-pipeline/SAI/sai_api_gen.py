80eb0e72d0bbf1fc2e0cceb529694887dfbd4420
r12f <r12f.code@gmail.com>
2023-12-13 18:22:11 +0000

    clean up script.

--- dash-pipeline/SAI/sai_api_gen.py
+++ dash-pipeline/SAI/sai_api_gen.py
@@ -123,7 +123,7 @@ class SAITypeSolver:
         return SAITypeSolver.sai_type_info_registry[sai_type]
 
     @staticmethod
-    def get_object_sai_type(object_size, object_name):
+    def get_object_sai_type(object_size):
         sai_type_name = ""
 
         if object_size == 1:
@@ -146,9 +146,9 @@ class SAITypeSolver:
         return SAITypeSolver.get_sai_type(sai_type_name)
 
     @staticmethod
-    def get_match_key_sai_type(match_type, key_size, key_name):
+    def get_match_key_sai_type(match_type, key_size):
         if match_type == 'exact' or match_type == 'optional' or match_type == 'ternary':
-            return  SAITypeSolver.get_object_sai_type(key_size, key_name)
+            return  SAITypeSolver.get_object_sai_type(key_size)
         elif match_type == 'lpm':
             return SAITypeSolver.__get_lpm_match_key_sai_type(key_size)
         elif match_type == 'list':
@@ -406,7 +406,7 @@ class SAIAPITableKey(SAIObject):
         if self.type != None:
             sai_type_info = SAITypeSolver.get_sai_type(self.type)
         else:
-            sai_type_info = SAITypeSolver.get_match_key_sai_type(self.match_type, self.bitwidth, self.name)
+            sai_type_info = SAITypeSolver.get_match_key_sai_type(self.match_type, self.bitwidth)
             self.type = sai_type_info.name
 
         self.field = sai_type_info.field_func_prefix
@@ -484,7 +484,7 @@ class SAIAPITableActionParam(SAIObject):
         if self.type != None:
             sai_type_info = SAITypeSolver.get_sai_type(self.type)
         else:
-            sai_type_info = SAITypeSolver.get_object_sai_type(self.bitwidth, self.name)
+            sai_type_info = SAITypeSolver.get_object_sai_type(self.bitwidth)
             self.type = sai_type_info.name
 
         self.field = sai_type_info.field_func_prefix
