a16fccf13876a896a26d7f2d831537ba94975be0
r12f <r12f.code@gmail.com>
2024-05-31 15:55:10 +0000

    Make the specs per API group file.

--- dash-pipeline/SAI/sai_api_gen.py
+++ dash-pipeline/SAI/sai_api_gen.py
@@ -10,10 +10,12 @@ try:
     import typing
     import base64
     import yaml
+    import yaml_include
     import jsonpath_ng.ext as jsonpath_ext
     import jsonpath_ng as jsonpath
     from utils.dash_p4 import DashP4SAIExtensions
     from utils.p4ir import P4IRTree, P4VarRefGraph
+    from utils.sai_spec import SaiSpec
     from utils.sai_gen import SAIGenerator
 except ImportError as ie:
     print("Import failed for " + ie.name)
@@ -28,15 +30,15 @@ if __name__ == "__main__":
     parser.add_argument("--ir", type=str, help="Path to P4 program IR JSON file")
     parser.add_argument("--print-sai-lib", type=bool)
     parser.add_argument("--ignore-tables", type=str, default="", help="Comma separated list of tables to ignore")
-    parser.add_argument("--output-sai-spec", type=str, help="Path to output SAI spec file")
+    parser.add_argument("--sai-spec-dir", type=str, required=True, help="Path to output SAI spec file")
     args = parser.parse_args()
+
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     p4rt_file_path = os.path.realpath(args.filepath)
     if not os.path.isfile(p4rt_file_path):
         print("File " + p4rt_file_path + " does not exist")
         exit(1)
-
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     p4ir = P4IRTree.from_file(args.ir)
     var_ref_graph = P4VarRefGraph(p4ir)
@@ -51,12 +53,22 @@ if __name__ == "__main__":
         print("Dumping parsed SAI data:")
         print(json.dumps(dash_sai_exts, indent=2))
 
-    # Output SAI spec
-    sai_spec = dash_sai_exts.to_sai()
-    if args.output_sai_spec:
-        output_sai_spec_path = os.path.realpath(args.output_sai_spec)
-        with open(output_sai_spec_path, "w") as f:
-            f.write(yaml.dump(sai_spec, indent=2, sort_keys=False))
+    # Initialize YAML loader and dumper
+    sai_spec_dir = os.path.realpath(args.sai_spec_dir)
+    yaml_inc_ctor = yaml_include.Constructor(base_dir=sai_spec_dir, autoload=True)
+    yaml.add_constructor("!inc", yaml_inc_ctor)
+    yaml_inc_rpr = yaml_include.Representer("inc")
+    yaml.add_representer(yaml_include.Data, yaml_inc_rpr)
+
+    # Ensure the current SAI spec can be loaded
+    print("Loading SAI spec from " + sai_spec_dir)
+    sai_spec = SaiSpec.deserialize(sai_spec_dir)
+
+    # Output the new SAI spec
+    print("Outputting new SAI spec to " + sai_spec_dir)
+    yaml_inc_ctor.autoload = False
+    new_sai_spec = dash_sai_exts.to_sai()
+    new_sai_spec.serialize(sai_spec_dir)
 
     # Generate and update all SAI files
     SAIGenerator(dash_sai_exts).generate()
--- dash-pipeline/SAI/utils/sai_spec/sai_spec.py
+++ dash-pipeline/SAI/utils/sai_spec/sai_spec.py
@@ -1,3 +1,6 @@
+import os
+import yaml
+import yaml_include
 from typing import List
 from .sai_enum import SaiEnum
 from .sai_api_group import SaiApiGroup
@@ -17,3 +20,26 @@ class SaiSpec:
         self.enums: List[SaiEnum] = []
         self.port_extenstion: SaiApiExtension = SaiApiExtension()
         self.api_groups: List[SaiApiGroup] = []
+
+    def serialize(self, spec_dir: str):
+        yaml_inc_files = []
+        for api_group in self.api_groups:
+            sai_api_group_spec_file_path = os.path.join(spec_dir, api_group.name + ".yaml")
+
+            with open(sai_api_group_spec_file_path, "w") as f:
+                f.write(yaml.dump(api_group, indent=2, sort_keys=False))
+            
+            yaml_inc_files.append(yaml_include.Data(urlpath=sai_api_group_spec_file_path))
+
+        api_groups = self.api_groups
+        self.api_groups = yaml_inc_files
+
+        sai_spec_file_path = os.path.join(spec_dir, "sai_spec.yaml")
+        with open(sai_spec_file_path, "w") as f:
+            f.write(yaml.dump(self, indent=2, sort_keys=False))
+
+        self.api_groups = api_groups
+
+    def deserialize(spec_dir: str):
+        with open(os.path.join(spec_dir, "sai_spec.yaml")) as f:
+            return yaml.unsafe_load(f)
