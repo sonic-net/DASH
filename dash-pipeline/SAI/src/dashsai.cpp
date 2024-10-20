d0400c9833f20d2f9f0ba0c9fb5d8b20512147de
r12f <r12f.code@gmail.com>
2024-10-20 14:43:07 -0700

    Make helper function for bulk object create and removal to simplify libsai generation.

--- dash-pipeline/SAI/src/dashsai.cpp
+++ dash-pipeline/SAI/src/dashsai.cpp
@@ -904,3 +904,60 @@ std::vector<sai_attribute_t> DashSai::populateDefaultAttributes(
 
     return attrs;
 }
+
+sai_status_t DashSai::bulk_create_objects(
+    _In_ DashSai::sai_create_object_fn create_fn,
+    _In_ sai_object_id_t switch_id,
+    _In_ uint32_t object_count,
+    _In_ const uint32_t *attr_count,
+    _In_ const sai_attribute_t **attr_list,
+    _In_ sai_bulk_op_error_mode_t mode,
+    _Out_ sai_object_id_t *object_id,
+    _Out_ sai_status_t *object_statuses)
+{
+    sai_status_t agg_status = SAI_STATUS_SUCCESS;
+
+    for (uint32_t i = 0; i < object_count; i++)
+    {
+        object_statuses[i] = create_fn(&object_id[i], switch_id, attr_count[i], attr_list[i]);
+
+        if (object_statuses[i] != SAI_STATUS_SUCCESS)
+        {
+            agg_status = SAI_STATUS_FAILURE;
+        }
+
+        if (agg_status == SAI_STATUS_FAILURE && mode == SAI_BULK_OP_ERROR_MODE_STOP_ON_ERROR)
+        {
+            return agg_status;
+        }
+    }
+
+    return agg_status;
+}
+
+sai_status_t DashSai::bulk_remove_objects(
+    _In_ DashSai::sai_remove_object_fn remove_fn,
+    _In_ uint32_t object_count,
+    _In_ const sai_object_id_t *object_id,
+    _In_ sai_bulk_op_error_mode_t mode,
+    _Out_ sai_status_t *object_statuses)
+{
+    sai_status_t agg_status = SAI_STATUS_SUCCESS;
+
+    for (uint32_t i = 0; i < object_count; i++)
+    {
+        object_statuses[i] = remove_fn(object_id[i]);
+
+        if (object_statuses[i] != SAI_STATUS_SUCCESS)
+        {
+            agg_status = SAI_STATUS_FAILURE;
+        }
+
+        if (agg_status == SAI_STATUS_FAILURE && mode == SAI_BULK_OP_ERROR_MODE_STOP_ON_ERROR)
+        {
+            return agg_status;
+        }
+    }
+
+    return agg_status;
+}
