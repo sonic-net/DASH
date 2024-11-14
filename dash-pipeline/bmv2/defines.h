73d3fa8e877247221787e4220718922f004940f8
Junhua Zhai <junhua.zhai@outlook.com>
2024-11-14 02:54:44 +0000

    Update defines.h

--- dash-pipeline/bmv2/defines.h
+++ dash-pipeline/bmv2/defines.h
@@ -1,13 +1,17 @@
 #ifndef _DASH_DEFINES_H_
 #define _DASH_DEFINES_H_
 
-#ifdef TABLE_FULL_SCALE
+#if defined(TABLE_HERO_SCALE)
 #define TABLE_CA_TO_PA_SIZE     (8 * 1024 * 1024)
 #define TABLE_ROUTING_SIZE      (4 * 1024 * 1024)
 
-#else   /* default size */
-#define TABLE_CA_TO_PA_SIZE     (8 * 1024)
-#define TABLE_ROUTING_SIZE      (4 * 1024)
+#elif defined(TABLE_BABY_HERO_SCALE)
+#define TABLE_CA_TO_PA_SIZE     (8 * 1024 * 10)
+#define TABLE_ROUTING_SIZE      (4 * 1024 * 10)
+
+#else   /* default/minimum size */
+#define TABLE_CA_TO_PA_SIZE     1024
+#define TABLE_ROUTING_SIZE      1024
 
 #endif
 
